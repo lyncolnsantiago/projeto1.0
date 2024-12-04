f510f880b7e8e79dab2af7595cccc5acaeb779f7
Lyncoln Santiago <lyncolnsantiago7@gmail.com>
2024-12-03 23:26:37 -0300

    meusite

---
+++ teladelogin.js
@@ -0,0 +1,1 @@
+function Entrar(){var a=document.getElementById("usuario").value,b=document.getElementById("senha").value;"lyncoln"==a&&"1234"==b?location.href="inicio.html":alert("usuario ou senha incorretos")}
